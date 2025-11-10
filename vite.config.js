ffc0f5096c59b2ffe5f71e65e387f89db4abf446
Santiago Urrea <santiago151120@gmail.com>
2025-11-09 23:04:25 -0500

    FIX URGENTE FINAL: Configurada compilacion de Tailwind en vite.config.js.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Asegúrate de que las rutas a los archivos de configuración sean correctas
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss, // Carga Tailwind CSS
+        autoprefixer // Carga Autoprefixer
+      ],
+    },
+  },
+});
